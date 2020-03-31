3736bc318047c5cfb4d5562ac0f90502726b4cfd
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-03-31 15:51:58 +0100

    Build folder completed for deployment

---
+++ build/precache-manifest.280de077ab189f341c4895e9764a4073.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f870d2b886095ef56acc15e34b9102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd5a85d59a42e9053c0",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "f6a19bee06ad807f3939",
+    "url": "/static/css/main.7b958768.chunk.css"
+  },
+  {
+    "revision": "cdd5a85d59a42e9053c0",
+    "url": "/static/js/2.5a4ed138.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5a4ed138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a19bee06ad807f3939",
+    "url": "/static/js/main.1c9fb782.chunk.js"
+  },
+  {
+    "revision": "cb9e5613ea0d7f00c58d",
+    "url": "/static/js/runtime-main.237f18d8.js"
+  },
+  {
+    "revision": "d7c221ed2342ec49fbeef049fb5c5d94",
+    "url": "/static/media/about-main2.d7c221ed.jpg"
+  },
+  {
+    "revision": "3df49c172d6d9259978884d1088dab1f",
+    "url": "/static/media/about1.3df49c17.jpg"
+  },
+  {
+    "revision": "59c53885e637e7644743d4ec8c2cbc71",
+    "url": "/static/media/about2.59c53885.jpg"
+  },
+  {
+    "revision": "de8469d1496332a61c4621e2b44012d8",
+    "url": "/static/media/about3.de8469d1.jpg"
+  },
+  {
+    "revision": "b847f7f79643ad5524340ed3e4d3359f",
+    "url": "/static/media/about4.b847f7f7.jpg"
+  },
+  {
+    "revision": "f628dccae3c83d9643c960a4f2aea781",
+    "url": "/static/media/about5.f628dcca.jpg"
+  },
+  {
+    "revision": "68deb62a5c3d2276fd168cfac0fcc35e",
+    "url": "/static/media/about6.68deb62a.jpg"
+  },
+  {
+    "revision": "4d18bbfd09eed2ab3221a33df8fabe77",
+    "url": "/static/media/contact-main2.4d18bbfd.jpg"
+  },
+  {
+    "revision": "0156a20b8296c293bbedff2f3201cc2e",
+    "url": "/static/media/hidden-venture-logo.0156a20b.PNG"
+  },
+  {
+    "revision": "440b053d6804a2c8d0975aaa837f471e",
+    "url": "/static/media/main1.440b053d.jpg"
+  },
+  {
+    "revision": "4c773480361dccef3652f0eb4820cded",
+    "url": "/static/media/main2.4c773480.jpg"
+  },
+  {
+    "revision": "331fc4b04235ad43e7df82d93f1563a0",
+    "url": "/static/media/services-main3.331fc4b0.jpg"
+  }
+]);
